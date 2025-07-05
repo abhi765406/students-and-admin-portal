90b0e6742fd4369dd591276eda52bf885fbf2603
Abhishek Kumar <abhi765406@gmail.com>
2025-07-05 22:36:25 +0530

    Redirect root URL to /students/

--- college_portal/urls.py
+++ college_portal/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('students/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='students/password_reset_done.html'), name='password_reset_done'),
     path('students/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='students/password_reset_confirm.html'), name='password_reset_confirm'),
     path('students/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='students/password_reset_complete.html'), name='password_reset_complete'),
+    path('', RedirectView.as_view(url='/students/', permanent=False)),  # Add this line
 ]
 
 # Media URL serving in development
